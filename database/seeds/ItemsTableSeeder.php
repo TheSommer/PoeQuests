0a1656281646c34e7a08891d1007d1d744838bfc
Rasmus Sommer <sommerawesome@gmail.com>
2018-02-09 22:44:11 +0100

    Added Items to ItemsTableSeeder.

--- database/seeds/ItemsTableSeeder.php
+++ database/seeds/ItemsTableSeeder.php
@@ -20,6 +20,19 @@ class ItemsTableSeeder extends Seeder
         $this->createItem('Bear Trap', 4, 0, 0, 16, ["Trap", "Duration", "Cast"]);
         $this->createItem('Blade Flurry', 28, 0, 29, 42, ["Attack", "AoE", "Channelling", "Melee"]);
         $this->createItem('Blade Vortex', 12, 0, 0, 33, ["Spell", "AoE", "Duration"]);
+        $this->createItem('Bladefall', 28, 0, 0, 67, ["Spell", "AoE"]);
+        $this->createItem('Blast Rain', 28, 0, 0, 67, ["Fire", "Attack", "AoE", "Bow"]);
+        $this->createItem('Blink Arrow', 10, 0, 0, 29, ["Attack", "Minion", "Duration", "Movement", "Bow"]);
+        $this->createItem('Blood Rage', 16, 0, 0, 41, ["Spell", "Duration"]);
+        $this->createItem('Burning Arrow', 1, 0, 0, 0, ["Attack", "Fire", "Bow"]);
+        $this->createItem('Caustic Arrow', 4, 0, 0, 16, ["Attack", "AoE", "Duration", "Chaos", "Bow"]);
+        $this->createItem('Charged Dash', 28, 0, 0, 67, ["Movement", "AoE", "Channelling", "Attack", "Lightning", "Melee"]);
+        $this->createItem('Cremation', 28, 0, 29, 42, ["Projectile", "Spell", "AoE", "Fire", "Duration"]);
+        $this->createItem('Cyclone', 28, 29, 0, 42, ["Attack", "AoE", "Movement", "Melee"]);
+        $this->createItem('Desecrate', 16, 0, 18, 26, ["Spell", "AoE", "Duration", "Chaos"]);
+        $this->createItem('Double Strike', 1, 0, 0, 0, ["Attack", "Melee"]);
+        $this->createItem('Dual Strike', 1, 0, 0, 0, ["Attack", "Melee"]);
+        $this->createItem('Elemental Hit', 1, 0, 0, 0, ["Attack", "Melee", "Fire", "Cold", "Lightning", "Bow"]);
     }
 
     public function createItem($name, $req_level, $req_str, $req_int, $req_dex, $str_tags){
