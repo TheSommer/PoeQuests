3ca1af2645418b36fd1defcf070b92c45bcc0bd2
Rasmus Sommer <sommerawesome@gmail.com>
2018-02-10 13:35:09 +0100

    Added requirements to items.index

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -5,14 +5,27 @@
 <table class="table">
   <tr>
     <th>Name</th>
+    <th>Requirements</th>
     <th>Tags</th>
   </tr>
   @foreach($items as $item)
     <tr>
       <td>{{ $item->name }}</td>
       <td>
+        <span class="badge badge-secondary">Lvl. {{ $item->req_level }}</span>
+        @if($item->req_str > 0)
+          <span class="badge badge-danger">{{ $item->req_str }} str</span>
+        @endif
+        @if($item->req_int > 0)
+          <span class="badge badge-primary">{{ $item->req_int }} int</span>
+        @endif
+        @if($item->req_dex > 0)
+          <span class="badge badge-success">{{ $item->req_dex }} dex</span>
+        @endif
+      </td>
+      <td>
         @foreach($item->getTags() as $tag)
-          <span class="badge badge-primary">{{ $tag->name }}</span> 
+          <span class="badge badge-dark">{{ $tag->name }}</span>
         @endforeach
       </td>
     </tr>
