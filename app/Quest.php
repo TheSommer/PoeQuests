49641a1d813e564a4f630c5bb5997d41175c08f3
Rasmus Sommer <sommerawesome@gmail.com>
2018-02-12 19:01:31 +0100

    Added getRewards() to Quest-model.

--- app/Quest.php
+++ app/Quest.php
@@ -3,8 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Character;
+use App\QuestReward;
 
 class Quest extends Model
 {
-    //
+    public function getRewards(){
+      $rewards = [];
+      $characters = Character::all();
+      foreach($characters as $character){
+        $char_rewards = QuestReward::where('character_id', $character->id)->where('quest_id', $this->id)->get();
+        array_push($rewards, $char_rewards);
+      }
+      return $rewards;
+    }
 }
